4ecb1d01a362d8f1ac3dc0c3c7084a84ef8cb1e8
poppyandarista <andarista999@gmail.com>
2025-01-08 19:04:40 +0700

    potrait layout

--- Laundry/app/src/main/java/com/luthfiana/laundry/MainActivity.kt
+++ Laundry/app/src/main/java/com/luthfiana/laundry/MainActivity.kt
@@ -1,17 +1,43 @@
 package com.luthfiana.laundry
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var tanggal: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        tanggal = findViewById(R.id.tanggal)
+
+        val dateFormat = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
+        val currentDate = dateFormat.format(Date())
+        tanggal.text = currentDate
+
+        val textSelamat: TextView = findViewById(R.id.selamat)
+
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+
+        val greeting = when (hour) {
+            in 5..11 -> "Selamat Pagi!"
+            in 12..15 -> "Selamat Siang!"
+            in 16..18 -> "Selamat Sore!"
+            else -> "Selamat Malam!"
+        }
+
+        textSelamat.text = "$greeting Poppy"
+
+ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
