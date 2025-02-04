9a0f3fb260dfa526fd30cc93cffaedae13365ce2
poppyandarista <andarista999@gmail.com>
2025-02-04 21:02:27 +0700

    added some activities and cardviews

---
+++ app/src/main/java/com/luthfiana/laundry/tambahan/DataTambahan.kt
@@ -0,0 +1,21 @@
+package com.luthfiana.laundry.tambahan
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.luthfiana.laundry.R
+
+class DataTambahan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_data_tambahan)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
